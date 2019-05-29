0d6f6f53a32e54a5f43300a63e806a3cbd4933c5
Jay Crumb <jay.crumb@tophat.com>
2019-05-28 20:22:34 -0400

    Add ml dev box

--- base.tf
+++ base.tf
@@ -1,4 +1,24 @@
 provider "google" {
 	project = "healthmoves"
 	region = "northamerica-northeast1"
+	zone = "northamerica-northeast1-a"
+}
+
+data "google_compute_image" "ml-dev-image" {
+	family = "ubuntu-1804-lts"
+	project = "gce-uefi-images"
+}
+
+resource "google_compute_instance" "ml-dev-box" {
+	name = "ml-dev-server"
+	machine_type = "n1-standard-1"
+	boot_disk {
+		initialize_params {
+			image = "${data.google_compute_image.ml-dev-image.self_link}"
+		}
+	}
+	network_interface {
+		network = "default"
+		access_config {}
+	}
 }
