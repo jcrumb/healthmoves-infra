0ddd74ce2c51f95199dd597923cbdf36d51e1419
Jay Crumb <jay.crumb@tophat.com>
2019-05-30 22:57:45 -0400

    Add ssh keys to dev box

--- base.tf
+++ base.tf
@@ -21,6 +21,9 @@ resource "google_compute_instance" "ml_dev_box" {
 		network = "default"
 		access_config {}
 	}
+	metadata = {
+		sshKeys = "moves:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCWv04gdPHWU+6A611irsUgQrS2rbrRxXHdMT0ZmDgJ7lv5NZoICz2pnNS39OhpvZjyQYWx7fmVD/mQSdYXVvpr5nDWRnaGddSP2O8mLOYCHp2QTjxLIGRYG3BEr8LODeu6zrGKUbabTmhe8372m+I43QBF35vfEjbo9InsfWlzPKMEl3j/P2T6/4V1XZqSSFQIqqfGnRPNHbsBm5N+zQmTWmcWNLPOuziPF/GqPOzYWIjQ3QFVUPzEg+v0iuIvzwTEA3k+OIYBs25wVHYxPh4Yfla8D7zKSc4zTlCDmOMl8CCnRMx/swJumJ8LxHhrTGoEb9Kh8L4dm9qacXZMoGXn j\nmoves:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDVdG3sN87wcPMnJVei8xOUToQgL7qauZEZdYLjfRGULR0e3TvJarwATl+Z7kKRCBs1g+XWa7zrxvhKIPQb0iW0n7YD/ooO9kF7JXMvfuJqJVv4nvEia7NBe5rBTEQno0aAbTBJwRZ8S3tDN/v0DhD46MDth8Avfd/+aYRsO7tV56R2tUme7wQmOmiSyAcOSEuuk6fqcRxYrQNl04/7ubzgioB/KlD44VnlpkJIzwgrb35Nkj+yBklRXl+Dvr3e5gubdw5SMJS/O3zqK8O2Yqrz1c1FnlsmtiUNjUZu5Dbx3aCaW10RrTrfOwDvfFRdWf06VIG8IldMlTLMXJ9ideZJNDGr+3qYg94yZ+/uc9n5+4tPdf9uSCvzS4ScZnTmuk0qKK6Pb40Se6QDp5Zig6BKmLzlpUMBz+44++mnprLei6WU1+kshso5tVT+0vo2y+MYZzVIMYQ8HkP7fUSVsXRs+9apzhSgUREI/suDCeqaWPg4mbEGy0rpGMAIwleGD/M= a"
+	}
 }
 
 resource "random_id" "db_suffix" {
